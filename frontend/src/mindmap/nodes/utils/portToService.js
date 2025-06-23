30f418976f5dd5f09a69c278a1936c19d535310a
hexadivine <hexadivine@gmail.com>
2025-06-23 22:48:50 +1000

    fix(portToService.js): correct port handling and update return structure for nextNodeTypes

--- frontend/src/mindmap/nodes/utils/portToService.js
+++ frontend/src/mindmap/nodes/utils/portToService.js
@@ -1,14 +1,15 @@
 export function portToService(port) {
-    switch (port) {
-        case "80":
+    const portNum = parseInt(port);
+    switch (portNum) {
+        case 80:
             return {
                 name: "Web Server",
                 color: "#008CBB",
-                nextFlow: ["scanDirsNode", "scanSubDomainsNode"],
+                nextNodeTypes: ["scanSubDomainsNode", "scanDirsNode"],
             };
-        case "22":
-            return { name: "SSH Server", color: "#C6BD13" };
+        case 22:
+            return { name: "SSH Server", color: "#C6BD13", nextNodeTypes: [] };
         default:
-            return { name: "", color: "white" };
+            return { name: "", color: "white", nextNodeTypes: [] };
     }
 }
