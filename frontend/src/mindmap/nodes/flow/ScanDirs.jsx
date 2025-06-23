b2fff32489953a8eff732dffc60fe16d84356c15
hexadivine <hexadivine@gmail.com>
2025-06-23 22:48:22 +1000

    feat(ScanDirs.jsx): implement ScanDirs component with directory scanning functionality

--- frontend/src/mindmap/nodes/flow/ScanDirs.jsx
+++ frontend/src/mindmap/nodes/flow/ScanDirs.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Template from "./Template";
+import { useMindmapContext } from "../../../context/Mindmap";
 
-function ScanDirs() {
-    return <div>ScanDirs</div>;
+function ScanDirs({ id, color }) {
+    const { target, addNewNode } = useMindmapContext();
+
+    useEffect(() => {
+        if (!target) return;
+    }, []);
+
+    function newNodeLogic() {
+        // addNewNode(id, "serviceNode", { x: 500, y: yPosition }, { port: port[0] });
+    }
+
+    return (
+        <Template
+            task="Directories"
+            lineRegEx={/.*\[Status:.*\]/gm}
+            wordRegEx={/.*/}
+            newNodeLogic={newNodeLogic}
+            cmd={`ffuf -u http://${target}/FUZZ -w /usr/share/wordlists/dirb/common.txt`}
+            color={color}
+        />
+    );
 }
 
 export default ScanDirs;
