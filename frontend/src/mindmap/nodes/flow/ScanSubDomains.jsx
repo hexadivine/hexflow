28303002910ce5a179aea5e6c3b9b5d3399f6897
hexadivine <hexadivine@gmail.com>
2025-06-23 22:48:28 +1000

    feat(ScanSubDomains.jsx): implement ScanSubDomains component with subdomain scanning functionality

--- frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
+++ frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Template from "./Template";
+import { useMindmapContext } from "../../../context/Mindmap";
 
-function ScanSubDomains() {
-    return <div>ScanSubDomains</div>;
+function ScanSubDomains({ color }) {
+    const { target, addNewNode } = useMindmapContext();
+
+    useEffect(() => {
+        if (!target) return;
+    }, []);
+
+    function newNodeLogic() {
+        // addNewNode(id, "serviceNode", { x: 500, y: yPosition }, { port: port[0] });
+    }
+
+    return (
+        <Template
+            task="Sub Domain"
+            lineRegEx={/.*\[Status:.*\]/gm}
+            wordRegEx={/.*/}
+            newNodeLogic={newNodeLogic}
+            cmd={`ffuf -u http://${target}/ -H "Host: FUZZ.${target}" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -fc 301,302`}
+            color={color}
+        />
+    );
 }
 
 export default ScanSubDomains;
