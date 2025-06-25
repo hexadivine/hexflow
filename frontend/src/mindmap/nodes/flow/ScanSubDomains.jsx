d74979f10f35f475f21688be45aa52bf12691ebb
hexadivine <hexadivine@gmail.com>
2025-06-25 16:50:53 +1000

    fix(ScanSubDomains.jsx): update newNodeLogic to accept id prop and adjust logic for newNode creation

--- frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
+++ frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Template from "./Template";
 import { useMindmapContext } from "../../../context/Mindmap";
 
-function ScanSubDomains({ color }) {
+function ScanSubDomains({ id, color }) {
     const { target, addNewNode } = useMindmapContext();
 
     useEffect(() => {
         if (!target) return;
     }, []);
 
-    function newNodeLogic() {
-        // addNewNode(id, "serviceNode", { x: 500, y: yPosition }, { port: port[0] });
-    }
+    function newNodeLogic() {}
 
     return (
         <Template
             task="Sub Domain"
             lineRegEx={/.*\[Status:.*\]/gm}
             wordRegEx={/.*/}
-            newNodeLogic={newNodeLogic}
+            newNodeLogic={"a"}
             cmd={`ffuf -u http://${target}/ -H "Host: FUZZ.${target}" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -fc 301,302`}
             color={color}
         />
