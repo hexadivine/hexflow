d3bce100493503fa59c22b1905d1239e206b5c37
hexadivine <hexadivine@gmail.com>
2025-06-25 16:50:46 +1000

    fix(ScanDirs.jsx): enhance newNodeLogic to handle .git data and manage yPosition

--- frontend/src/mindmap/nodes/flow/ScanDirs.jsx
+++ frontend/src/mindmap/nodes/flow/ScanDirs.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Template from "./Template";
 import { useMindmapContext } from "../../../context/Mindmap";
 
 function ScanDirs({ id, color }) {
     const { target, addNewNode } = useMindmapContext();
+    const yPosition = useRef(-200);
 
     useEffect(() => {
         if (!target) return;
     }, []);
 
-    function newNodeLogic() {
-        // addNewNode(id, "serviceNode", { x: 500, y: yPosition }, { port: port[0] });
+    function newNodeLogic(data) {
+        console.log("hi");
+        console.log(data);
+        console.log(data.includes(".git"));
+        if (data.includes(".git"))
+            addNewNode(
+                id,
+                "terminalNode",
+                { x: 700, y: yPosition.current || -200 },
+                {
+                    command: `python3 /opt/git-dumper/git_dumper.py http://${target} /${target} && cd /${target} && tree`,
+                }
+            );
+        yPosition.current += 300;
     }
 
     return (
