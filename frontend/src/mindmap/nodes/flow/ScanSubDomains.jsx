5564129b37f9574def16ca4d3b5f7f11dafc77c5
hexadivine <hexadivine@gmail.com>
2025-06-26 21:17:42 +1000

    fix(ScanSubDomains.jsx): update command to filter 301 and 200 status codes; remove unused useEffect hook

--- frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
+++ frontend/src/mindmap/nodes/flow/ScanSubDomains.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useRef } from "react";
 import Template from "./Template";
 import { useMindmapContext } from "../../../context/Mindmap";
+import { isValidIP } from "../utils/validateTarget";
 
 function ScanSubDomains({ id, color }) {
     const { target, addNewNode } = useMindmapContext();
 
-    useEffect(() => {
-        if (!target) return;
-    }, []);
-
     function newNodeLogic() {}
+
+    if (!target) return;
+    if (isValidIP(target))
+        return (
+            <p style={{ background: color }} className="p-2 rounded-2xl">
+                IP cannot be enumerated for Subdomains
+            </p>
+        );
 
     return (
         <Template
             task="Sub Domain"
             lineRegEx={/.*\[Status:.*\]/gm}
             wordRegEx={/.*/}
-            newNodeLogic={"a"}
-            cmd={`ffuf -u http://${target}/ -H "Host: FUZZ.${target}" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -fc 301`}
+            newNodeLogic={newNodeLogic}
+            cmd={`ffuf -u http://${target}/ -H "Host: FUZZ.${target}" -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -fc 301,200`}
             color={color}
         />
     );
